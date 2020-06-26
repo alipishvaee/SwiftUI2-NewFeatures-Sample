4799ece9702afb2ef5a9d5ad112ec20c40305fa4
Ali Pishvaee <alipishvaee2@Gmail.com>
2020-06-26 23:08:40 +0430

    Initial Commit

---
+++ SwiftUI2 NewFeatures Sample/SwiftUI2_NewFeatures_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI2_NewFeatures_SampleApp.swift
+//  SwiftUI2 NewFeatures Sample
+//
+//  Created by Ali Pishvaee on 6/26/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI2_NewFeatures_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI2 NewFeatures SampleTests/SwiftUI2_NewFeatures_SampleTests.swift
@@ -0,0 +1,33 @@
+//
+//  SwiftUI2_NewFeatures_SampleTests.swift
+//  SwiftUI2 NewFeatures SampleTests
+//
+//  Created by Ali Pishvaee on 6/26/20.
+//
+
+import XCTest
+@testable import SwiftUI2_NewFeatures_Sample
+
+class SwiftUI2_NewFeatures_SampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
