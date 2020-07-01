2e439e0e7186d5e0f28d47ea4833dee465d7632c
Ali Pishvaee <alipishvaee2@Gmail.com>
2020-07-01 20:55:52 +0430

    Arrangement in MapView

--- SwiftUI2 NewFeatures Sample/NewFeatures/MapView.swift
+++ SwiftUI2 NewFeatures Sample/NewFeatures/MapView.swift
@@ -8,7 +8,9 @@ import MapKit
 import SwiftUI
 
 struct MapView: View {
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.4353, longitude: 35.23442), span: MKCoordinateSpan(latitudeDelta: 0.15, longitudeDelta: 0.15))
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 51.4353, longitude: 35.23442),
+        span: MKCoordinateSpan(latitudeDelta: 0.15, longitudeDelta: 0.15))
     var body: some View {
         Map(coordinateRegion: $region)
     }
